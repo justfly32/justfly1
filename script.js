aed44184d60103e144ed850221d01f764bd2443a
justfly32 <justfly32@gmail.com>
2024-07-17 10:53:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Custom JavaScript can be added here
-console.log("Welcome to My GitHub Pages Site!");
+// Smooth scrolling anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
